f1ef8e6ef84810841aaf7c1c23ec6232697200ce
adizbek <adizbek1998@gmail.com>
2019-07-23 11:42:11 +0500

    Fixed bugs.
    Long text wasn't translating. Fixed
    Url stays the same while switching to POST, actually should remove q parameter from url. Fixed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const tokenGenerator = require('./tokenGenerator');
 const querystring = require('querystring');
 const got = require('got');
 
-
 /**
  * @function translate
  * @param {String} text The text to be translated.
@@ -41,7 +40,7 @@ async function translate(text, options) {
     let token = await tokenGenerator.generate(text);
 
     // URL & query string required by Google Translate.
-    let url = 'https://translate.google.com/translate_a/single';
+    let baseUrl = 'https://translate.google.com/translate_a/single';
     let data = {
       client: 'gtx',
       sl: options.from,
@@ -59,19 +58,20 @@ async function translate(text, options) {
     };
 
     // Append query string to the request URL.
-    url = `${url}?${querystring.stringify(data)}`;
+    let url = `${baseUrl}?${querystring.stringify(data)}`;
 
     let requestOptions;
     // If request URL is greater than 2048 characters, use POST method.
     if (url.length > 2048) {
       delete data.q;
       requestOptions = [
-        `${url}?${querystring.stringify(data)}`,
+        `${baseUrl}?${querystring.stringify(data)}`,
         {
           method: 'POST',
-          body: JSON.stringify({
+          form: true,
+          body: {
             q: text
-          })
+          }
         }
       ];
     }
