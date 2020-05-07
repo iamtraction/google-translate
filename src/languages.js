15a1a53d40305eadf7d1d7d8d4be27b4e1d84877
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2020-05-07 22:12:32 +0530

    update refactor
    Signed-off-by: Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>

--- src/languages.js
+++ src/languages.js
@@ -1,117 +1,118 @@
 /**
  * Generated from https://translate.google.com
  *
- * The languages that Google Translate supports (as of 2/11/2018) alongside
+ * The languages that Google Translate supports (as of 7/5/2020) alongside
  * their ISO 639-1 codes
- * See https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+ * @see https://cloud.google.com/translate/docs/languages
+ * @see https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
  */
 
-let languages = {
-  'auto': 'Automatic',
-  'af': 'Afrikaans',
-  'sq': 'Albanian',
-  'am': 'Amharic',
-  'ar': 'Arabic',
-  'hy': 'Armenian',
-  'az': 'Azerbaijani',
-  'eu': 'Basque',
-  'be': 'Belarusian',
-  'bn': 'Bengali',
-  'bs': 'Bosnian',
-  'bg': 'Bulgarian',
-  'ca': 'Catalan',
-  'ceb': 'Cebuano',
-  'ny': 'Chichewa',
-  'zh-cn': 'Chinese Simplified',
-  'zh-tw': 'Chinese Traditional',
-  'co': 'Corsican',
-  'hr': 'Croatian',
-  'cs': 'Czech',
-  'da': 'Danish',
-  'nl': 'Dutch',
-  'en': 'English',
-  'eo': 'Esperanto',
-  'et': 'Estonian',
-  'tl': 'Filipino',
-  'fi': 'Finnish',
-  'fr': 'French',
-  'fy': 'Frisian',
-  'gl': 'Galician',
-  'ka': 'Georgian',
-  'de': 'German',
-  'el': 'Greek',
-  'gu': 'Gujarati',
-  'ht': 'Haitian Creole',
-  'ha': 'Hausa',
-  'haw': 'Hawaiian',
-  'iw': 'Hebrew',
-  'hi': 'Hindi',
-  'hmn': 'Hmong',
-  'hu': 'Hungarian',
-  'is': 'Icelandic',
-  'ig': 'Igbo',
-  'id': 'Indonesian',
-  'ga': 'Irish',
-  'it': 'Italian',
-  'ja': 'Japanese',
-  'jw': 'Javanese',
-  'kn': 'Kannada',
-  'kk': 'Kazakh',
-  'km': 'Khmer',
-  'ko': 'Korean',
-  'ku': 'Kurdish (Kurmanji)',
-  'ky': 'Kyrgyz',
-  'lo': 'Lao',
-  'la': 'Latin',
-  'lv': 'Latvian',
-  'lt': 'Lithuanian',
-  'lb': 'Luxembourgish',
-  'mk': 'Macedonian',
-  'mg': 'Malagasy',
-  'ms': 'Malay',
-  'ml': 'Malayalam',
-  'mt': 'Maltese',
-  'mi': 'Maori',
-  'mr': 'Marathi',
-  'mn': 'Mongolian',
-  'my': 'Myanmar (Burmese)',
-  'ne': 'Nepali',
-  'no': 'Norwegian',
-  'ps': 'Pashto',
-  'fa': 'Persian',
-  'pl': 'Polish',
-  'pt': 'Portuguese',
-  'pa': 'Punjabi',
-  'ro': 'Romanian',
-  'ru': 'Russian',
-  'sm': 'Samoan',
-  'gd': 'Scots Gaelic',
-  'sr': 'Serbian',
-  'st': 'Sesotho',
-  'sn': 'Shona',
-  'sd': 'Sindhi',
-  'si': 'Sinhala',
-  'sk': 'Slovak',
-  'sl': 'Slovenian',
-  'so': 'Somali',
-  'es': 'Spanish',
-  'su': 'Sundanese',
-  'sw': 'Swahili',
-  'sv': 'Swedish',
-  'tg': 'Tajik',
-  'ta': 'Tamil',
-  'te': 'Telugu',
-  'th': 'Thai',
-  'tr': 'Turkish',
-  'uk': 'Ukrainian',
-  'ur': 'Urdu',
-  'uz': 'Uzbek',
-  'vi': 'Vietnamese',
-  'cy': 'Welsh',
-  'xh': 'Xhosa',
-  'yi': 'Yiddish',
-  'yo': 'Yoruba',
-  'zu': 'Zulu'
+const languages = {
+    "auto": "Automatic",
+    "af": "Afrikaans",
+    "sq": "Albanian",
+    "am": "Amharic",
+    "ar": "Arabic",
+    "hy": "Armenian",
+    "az": "Azerbaijani",
+    "eu": "Basque",
+    "be": "Belarusian",
+    "bn": "Bengali",
+    "bs": "Bosnian",
+    "bg": "Bulgarian",
+    "ca": "Catalan",
+    "ceb": "Cebuano",
+    "ny": "Chichewa",
+    "zh-cn": "Chinese Simplified",
+    "zh-tw": "Chinese Traditional",
+    "co": "Corsican",
+    "hr": "Croatian",
+    "cs": "Czech",
+    "da": "Danish",
+    "nl": "Dutch",
+    "en": "English",
+    "eo": "Esperanto",
+    "et": "Estonian",
+    "tl": "Filipino",
+    "fi": "Finnish",
+    "fr": "French",
+    "fy": "Frisian",
+    "gl": "Galician",
+    "ka": "Georgian",
+    "de": "German",
+    "el": "Greek",
+    "gu": "Gujarati",
+    "ht": "Haitian Creole",
+    "ha": "Hausa",
+    "haw": "Hawaiian",
+    "iw": "Hebrew",
+    "hi": "Hindi",
+    "hmn": "Hmong",
+    "hu": "Hungarian",
+    "is": "Icelandic",
+    "ig": "Igbo",
+    "id": "Indonesian",
+    "ga": "Irish",
+    "it": "Italian",
+    "ja": "Japanese",
+    "jw": "Javanese",
+    "kn": "Kannada",
+    "kk": "Kazakh",
+    "km": "Khmer",
+    "ko": "Korean",
+    "ku": "Kurdish (Kurmanji)",
+    "ky": "Kyrgyz",
+    "lo": "Lao",
+    "la": "Latin",
+    "lv": "Latvian",
+    "lt": "Lithuanian",
+    "lb": "Luxembourgish",
+    "mk": "Macedonian",
+    "mg": "Malagasy",
+    "ms": "Malay",
+    "ml": "Malayalam",
+    "mt": "Maltese",
+    "mi": "Maori",
+    "mr": "Marathi",
+    "mn": "Mongolian",
+    "my": "Myanmar (Burmese)",
+    "ne": "Nepali",
+    "no": "Norwegian",
+    "ps": "Pashto",
+    "fa": "Persian",
+    "pl": "Polish",
+    "pt": "Portuguese",
+    "pa": "Punjabi",
+    "ro": "Romanian",
+    "ru": "Russian",
+    "sm": "Samoan",
+    "gd": "Scots Gaelic",
+    "sr": "Serbian",
+    "st": "Sesotho",
+    "sn": "Shona",
+    "sd": "Sindhi",
+    "si": "Sinhala",
+    "sk": "Slovak",
+    "sl": "Slovenian",
+    "so": "Somali",
+    "es": "Spanish",
+    "su": "Sundanese",
+    "sw": "Swahili",
+    "sv": "Swedish",
+    "tg": "Tajik",
+    "ta": "Tamil",
+    "te": "Telugu",
+    "th": "Thai",
+    "tr": "Turkish",
+    "uk": "Ukrainian",
+    "ur": "Urdu",
+    "uz": "Uzbek",
+    "vi": "Vietnamese",
+    "cy": "Welsh",
+    "xh": "Xhosa",
+    "yi": "Yiddish",
+    "yo": "Yoruba",
+    "zu": "Zulu"
 };
 
 /**
@@ -122,17 +123,17 @@ let languages = {
  * language is not supported
  */
 function getISOCode(language) {
-  if (!language) return false;
-  language = language.toLowerCase();
-  if (language in languages) return language;
+    if (!language) return false;
+    language = language.toLowerCase();
+    if (language in languages) return language;
 
-  let keys = Object.keys(languages).filter((key) => {
-    if (typeof languages[key] !== 'string') return false;
+    let keys = Object.keys(languages).filter((key) => {
+        if (typeof languages[key] !== "string") return false;
 
-    return languages[key].toLowerCase() === language;
-  });
+        return languages[key].toLowerCase() === language;
+    });
 
-  return keys[0] || null;
+    return keys[0] || null;
 }
 
 /**
@@ -141,7 +142,7 @@ function getISOCode(language) {
  * @returns {boolean} If the language is supported or not.
  */
 function isSupported(language) {
-  return Boolean(getISOCode(language));
+    return Boolean(getISOCode(language));
 }
 
 module.exports = languages;
