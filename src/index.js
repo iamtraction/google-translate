921cf8ac015769933e7bf5619fe95f6b069c479b
Sankarsan Kampa <19631364+iamtraction@users.noreply.github.com>
2021-10-09 16:46:14 +0530

    remove unecessary try/catch block
    Signed-off-by: Sankarsan Kampa <19631364+iamtraction@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-const languages = require("./languages");
-const tokenGenerator = require("./tokenGenerator");
 const querystring = require("querystring");
 const got = require("got");
+
+const languages = require("./languages");
+const tokenGenerator = require("./tokenGenerator");
 
 /**
  * @function translate
@@ -10,136 +11,131 @@ const got = require("got");
  * @returns {Object} The result containing the translation.
  */
 async function translate(text, options) {
-    try {
-        if (typeof options !== "object") options = {};
-        text = String(text);
+    if (typeof options !== "object") options = {};
+    text = String(text);
 
-        // Check if a lanugage is in supported; if not, throw an error object.
-        let error;
-        [ options.from, options.to ].forEach((lang) => {
-            if (lang && !languages.isSupported(lang)) {
-                error = new Error();
-                error.code = 400;
-                error.message = `The language '${lang}' is not supported.`;
+    // Check if a lanugage is in supported; if not, throw an error object.
+    let error;
+    [ options.from, options.to ].forEach((lang) => {
+        if (lang && !languages.isSupported(lang)) {
+            error = new Error();
+            error.code = 400;
+            error.message = `The language '${lang}' is not supported.`;
+        }
+    });
+    if (error) throw error;
+
+    // If options object doesn"t have "from" language, set it to "auto".
+    if (!Object.prototype.hasOwnProperty.call(options, "from")) options.from = "auto";
+    // If options object doesn"t have "to" language, set it to "en".
+    if (!Object.prototype.hasOwnProperty.call(options, "to")) options.to = "en";
+    // If options object has a "raw" property evaluating to true, set it to true.
+    options.raw = Boolean(options.raw);
+
+    // Get ISO 639-1 codes for the languages.
+    options.from = languages.getISOCode(options.from);
+    options.to = languages.getISOCode(options.to);
+
+    // Generate Google Translate token for the text to be translated.
+    let token = await tokenGenerator.generate(text);
+
+    // URL & query string required by Google Translate.
+    let baseUrl = "https://translate.google.com/translate_a/single";
+    let data = {
+        client: "gtx",
+        sl: options.from,
+        tl: options.to,
+        hl: options.to,
+        dt: [ "at", "bd", "ex", "ld", "md", "qca", "rw", "rm", "ss", "t" ],
+        ie: "UTF-8",
+        oe: "UTF-8",
+        otf: 1,
+        ssel: 0,
+        tsel: 0,
+        kc: 7,
+        q: text,
+        [token.name]: token.value
+    };
+
+    // Append query string to the request URL.
+    let url = `${baseUrl}?${querystring.stringify(data)}`;
+
+    let requestOptions;
+    // If request URL is greater than 2048 characters, use POST method.
+    if (url.length > 2048) {
+        delete data.q;
+        requestOptions = [
+            `${baseUrl}?${querystring.stringify(data)}`,
+            {
+                method: "POST",
+                form: {
+                    q: text
+                }
             }
-        });
-        if (error) throw error;
+        ];
+    }
+    else {
+        requestOptions = [ url ];
+    }
 
-        // If options object doesn"t have "from" language, set it to "auto".
-        if (!Object.prototype.hasOwnProperty.call(options, "from")) options.from = "auto";
-        // If options object doesn"t have "to" language, set it to "en".
-        if (!Object.prototype.hasOwnProperty.call(options, "to")) options.to = "en";
-        // If options object has a "raw" property evaluating to true, set it to true.
-        options.raw = Boolean(options.raw);
+    // Request translation from Google Translate.
+    let response = await got(...requestOptions);
 
-        // Get ISO 639-1 codes for the languages.
-        options.from = languages.getISOCode(options.from);
-        options.to = languages.getISOCode(options.to);
-
-        // Generate Google Translate token for the text to be translated.
-        let token = await tokenGenerator.generate(text);
-
-        // URL & query string required by Google Translate.
-        let baseUrl = "https://translate.google.com/translate_a/single";
-        let data = {
-            client: "gtx",
-            sl: options.from,
-            tl: options.to,
-            hl: options.to,
-            dt: [ "at", "bd", "ex", "ld", "md", "qca", "rw", "rm", "ss", "t" ],
-            ie: "UTF-8",
-            oe: "UTF-8",
-            otf: 1,
-            ssel: 0,
-            tsel: 0,
-            kc: 7,
-            q: text,
-            [token.name]: token.value
-        };
-
-        // Append query string to the request URL.
-        let url = `${baseUrl}?${querystring.stringify(data)}`;
-
-        let requestOptions;
-        // If request URL is greater than 2048 characters, use POST method.
-        if (url.length > 2048) {
-            delete data.q;
-            requestOptions = [
-                `${baseUrl}?${querystring.stringify(data)}`,
-                {
-                    method: "POST",
-                    form: {
-                        q: text
-                    }
-                }
-            ];
-        }
-        else {
-            requestOptions = [ url ];
-        }
-
-        // Request translation from Google Translate.
-        let response = await got(...requestOptions);
-
-        let result = {
-            text: "",
-            from: {
-                language: {
-                    didYouMean: false,
-                    iso: ""
-                },
-                text: {
-                    autoCorrected: false,
-                    value: "",
-                    didYouMean: false
-                }
+    let result = {
+        text: "",
+        from: {
+            language: {
+                didYouMean: false,
+                iso: ""
             },
-            raw: ""
-        };
-
-        // If user requested a raw output, add the raw response to the result
-        if (options.raw) {
-            result.raw = response.body;
-        }
-
-        // Parse string body to JSON and add it to result object.
-
-        let body = JSON.parse(response.body);
-        body[0].forEach((obj) => {
-            if (obj[0]) {
-                result.text += obj[0];
+            text: {
+                autoCorrected: false,
+                value: "",
+                didYouMean: false
             }
-        });
+        },
+        raw: ""
+    };
 
-        if (body[2] === body[8][0][0]) {
-            result.from.language.iso = body[2];
+    // If user requested a raw output, add the raw response to the result
+    if (options.raw) {
+        result.raw = response.body;
+    }
+
+    // Parse string body to JSON and add it to result object.
+
+    let body = JSON.parse(response.body);
+    body[0].forEach((obj) => {
+        if (obj[0]) {
+            result.text += obj[0];
+        }
+    });
+
+    if (body[2] === body[8][0][0]) {
+        result.from.language.iso = body[2];
+    }
+    else {
+        result.from.language.didYouMean = true;
+        result.from.language.iso = body[8][0][0];
+    }
+
+    if (body[7] && body[7][0]) {
+        let str = body[7][0];
+
+        str = str.replace(/<b><i>/g, "[");
+        str = str.replace(/<\/i><\/b>/g, "]");
+
+        result.from.text.value = str;
+
+        if (body[7][5] === true) {
+            result.from.text.autoCorrected = true;
         }
         else {
-            result.from.language.didYouMean = true;
-            result.from.language.iso = body[8][0][0];
+            result.from.text.didYouMean = true;
         }
-
-        if (body[7] && body[7][0]) {
-            let str = body[7][0];
-
-            str = str.replace(/<b><i>/g, "[");
-            str = str.replace(/<\/i><\/b>/g, "]");
-
-            result.from.text.value = str;
-
-            if (body[7][5] === true) {
-                result.from.text.autoCorrected = true;
-            }
-            else {
-                result.from.text.didYouMean = true;
-            }
-        }
-
-        return result;
     }
-    catch (e) {
-        throw e;
-    }
+
+    return result;
 }
 
 module.exports = translate;
